47cd98c39d8da6e8718f2f2f5a04dc8425ae0c70
kambaikariuki <kariukikamba@gmail.com>
2024-12-01 15:31:10 +0200

    .

---
+++ web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -0,0 +1,7 @@
+
+# copies file
+
+file {'copy_file':
+  ensure => file,
+  source => '/var/www/html/wp-includes/class-wp-locale.php',
+  path   => '/var/www/html/wp-includes/class-wp-locale.phpp',}
